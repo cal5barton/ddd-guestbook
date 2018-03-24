5edd1733a21253f43874296576b6d52f56ea5313
cal5fishbowl <callen.barton@fishbowlinventory.com>
2018-03-24 13:01:01 -0400

    Renamed assembly and fixed test so it passes.

--- tests/CleanArchitecture.Tests/Integration/Web/HomeControllerIndexShould.cs
+++ tests/CleanArchitecture.Tests/Integration/Web/HomeControllerIndexShould.cs
@@ -3,16 +3,16 @@ using Xunit;
 
 namespace DDDGuestbook.Tests.Integration.Web
 {
-    public class HomeControllerContactShould : BaseWebTest
+    public class HomeControllerIndexShould : BaseWebTest
     {
         [Fact]
         public async Task ReturnViewWithCorrectMessage()
         {
-            var response = await _client.GetAsync("/home/contact");
+            var response = await _client.GetAsync("/home");
             response.EnsureSuccessStatusCode();
             var stringResponse = await response.Content.ReadAsStringAsync();
 
-            Assert.Contains("Your contact page", stringResponse);
+            Assert.Contains("DDDGuestbook.Web", stringResponse);
 
         }
     }
